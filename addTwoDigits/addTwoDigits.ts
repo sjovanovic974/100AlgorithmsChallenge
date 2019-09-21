e1572872957fc3ed18c200cf0289a7b9602f539a
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-21 23:22:27 +0200

    Add Two Digits Exercise added

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,3 +1,14 @@
+// One line solution that works only if we are sure that argument number has only two digits
+function addTwoDigits(n: string|number): number {
+  return parseInt(n.toString()[0]) + parseInt(n.toString()[1]);
+}
+
+console.log(addTwoDigits(29));
+
+/*
+//Two solutions for a number that can have many digits
+
+//A modern solution using reduce() on an array 
 function addTwoDigits(n: string|number): number {
   return n.toString()
           .split('')
@@ -6,17 +17,18 @@ function addTwoDigits(n: string|number): number {
 
 console.log(addTwoDigits(29));
 
-// // Old School solution
-// function addTwoDigits(n: string|number): number {
-//   const numArr:string[] = n.toString().split('');
+// Old School solution
+function addTwoDigits(n: string|number): number {
+  const numArr:string[] = n.toString().split('');
 
-//   let total:number = 0;
+  let total:number = 0;
 
-//   for(let i = 0; i < numArr.length; i++){
-//     total += parseInt(numArr[i]);
-//   }
+  for(let i = 0; i < numArr.length; i++){
+    total += parseInt(numArr[i]);
+  }
 
-//   return total;
-// }
+  return total;
+}
 
-// console.log(addTwoDigits(12));
+console.log(addTwoDigits(12));
+*/
