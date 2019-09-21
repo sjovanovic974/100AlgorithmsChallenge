b0781c54b493265d04b9f25a5fdc591fa2220d72
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-21 14:28:34 +0200

    Reverse string exercise added

---
+++ reverseAString/reverseAString.js
@@ -0,0 +1,6 @@
+function reverseAString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseAString('hello'));
+console.log(reverseAString('Howdy'));
+
---
+++ reverseAString/reverseAString.ts
@@ -0,0 +1,22 @@
+// one line solution
+function reverseAString(str: string): string {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseAString('hello'));
+console.log(reverseAString('Howdy'));
+
+
+//old school solution
+// function reverseAString(str: string): string {
+//   let output: string = '';
+
+//   for(let i: number = str.length -1; i >= 0; i--){
+//     output += str[i];
+//   }
+
+//   return output;
+// }
+
+// console.log(reverseAString('hello'));
+// console.log(reverseAString('Howdy'));
