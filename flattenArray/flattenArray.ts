0056caf56398b1d412c95a68c59c84e87fdceb63
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-22 12:01:52 +0200

    Flatten Array Exercise added

---
+++ flattenArray/flattenArray.ts
@@ -0,0 +1,17 @@
+// My solution to the problem using "for loop" and "reduce()"
+function flattenArray(arr: any[]): any[] {
+
+  for(let i = 0; i < arr.length; i++){
+    while(Array.isArray(arr[i])){
+      arr = arr.reduce((acc, el) => {
+        return acc.concat(el);
+      }, []);
+    }
+  }
+
+  return arr;
+}
+
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([1, [4, [2, 5]]]));
