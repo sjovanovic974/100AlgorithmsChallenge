c791362dfc81896106d5bb340d1c8f049ad9bac6
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-23 23:33:28 +0200

    Turned the input string to lower case

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,6 +1,6 @@
 //Simple solution to check for palindromes
 function checkPalindrome(inputString: string): boolean {
-  return inputString === inputString.split('').reverse().join('');
+  return inputString.toLowerCase() === inputString.split('').reverse().join('').toLowerCase();
 }
 
 console.log(checkPalindrome('aabaa'));
@@ -10,6 +10,7 @@ console.log(checkPalindrome('a'));
 /*
 //old school solution
 function checkPalindrome(inputString: string): boolean {
+  inputString = inputString.toLowerCase();
   let a = 0, b = inputString.length -1;
   while(a < b){
     if(inputString[a] !== inputString[b]){
