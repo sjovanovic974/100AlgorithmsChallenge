73a82883c7acdef326b898a125989ffeee92fd24
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-24 21:04:15 +0200

    Are Similar Exercise added

---
+++ areSimilar/areSimilar.ts
@@ -0,0 +1,12 @@
+// Solution using sorting and conversion to string to enable
+// comparison of values
+function areSimilar(a: number[], b: number[]): boolean {
+   const sortedA = a.sort((x, y) => x - y);
+   const sortedB = b.sort((x, y) => x - y);
+   return sortedA.toString() === sortedB.toString();
+}
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([7, 3, 2], [2, 7, 3]));
