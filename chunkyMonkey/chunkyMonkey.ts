2fb6e867fae36c0b0585b187a9651d739ec858b6
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-26 00:23:46 +0200

    Better solution created for chunkyMonkey exercise

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,4 +1,18 @@
-// Using slice() to cut an array into desired number of subarrays 
+// Found this to be the most concise solution I could have thought of
+function chunkyMonkey(arr: any[], size: number): any[][] {
+  const tempArr: any[] = [];
+  
+  while (arr.length) {
+    tempArr.push(arr.splice(0, size));
+  }
+
+  return tempArr;
+}
+
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+
+// First solution: Using slice() to cut an array into desired number of subarrays 
 // function chunkyMonkey(arr: any[], size: number): any[][] {
 //   const tempArr: any[] = [];
 
@@ -8,15 +22,14 @@
 //   return tempArr;
 // }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
 
-function chunkyMonkey(arr: any[], size: number): any[][] {
-  const tempArr: any[] = [];
-  let count = 0;
+// Variation of the above solution with while loop
+// function chunkyMonkey(arr: any[], size: number): any[][] {
+//   const tempArr: any[] = [];
+//   let count = 0;
 
-  while (count < arr.length) {
-    tempArr.push(arr.slice(count, count += size));
-  }
-  return tempArr;
-}
+//   while (count < arr.length) {
+//     tempArr.push(arr.slice(count, count += size));
+//   }
+//   return tempArr;
+// }
