31801b4d09ec39ebe56faa373ac17c2ae6890c34
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-25 23:52:39 +0200

    Added alternative solution to chunkyMonkey exercises

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,11 +1,22 @@
-function chunkyMonkey(arr: any[], size: number): any[][] {
-  const tempArr: any[] = [];
+// Using slice() to cut an array into desired number of subarrays 
+// function chunkyMonkey(arr: any[], size: number): any[][] {
+//   const tempArr: any[] = [];
 
-  for(let i: number = 0; i < arr.length; i+= size) {
-    tempArr.push(arr.slice(i, i + size));
-  }
-  return tempArr;
-}
+//   for(let i: number = 0; i < arr.length; i+= size) {
+//     tempArr.push(arr.slice(i, i + size));
+//   }
+//   return tempArr;
+// }
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+
+function chunkyMonkey(arr: any[], size: number): any[][] {
+  const tempArr: any[] = [];
+  let count = 0;
+
+  while (count < arr.length) {
+    tempArr.push(arr.slice(count, count += size));
+  }
+  return tempArr;
+}
