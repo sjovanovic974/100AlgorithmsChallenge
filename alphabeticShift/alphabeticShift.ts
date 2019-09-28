cff79afedf18eefd86e59312fce948fced210803
sjovanovic974 <sjovanovic974@gmail.com>
2019-09-28 14:24:43 +0200

    Alphabetic Shift Exercise added

---
+++ alphabeticShift/alphabeticShift.ts
@@ -0,0 +1,15 @@
+// My solution of the problem using String methodes
+function alphabeticShift(inputString: string): string {
+  return inputString.split('').map( (l) => {
+    if (l === 'z') {
+      return 'a';
+    } else if(l === 'Z') {
+      return 'A'
+    } else {
+      return String.fromCharCode(l.charCodeAt(0) + 1);
+    }
+  }).join('');
+}
+
+console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('AAZZZ'));
