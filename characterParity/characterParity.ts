97646fa496284425d96f2428ddb2242c79834e04
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-02 21:07:13 +0200

    characterParity Exercise improved

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,10 +1,18 @@
 // First solution
-function characterParity(symbol: string): string {
-   if(isNaN(parseInt(symbol[0]))) return "not a digit"
-   else if (parseInt(symbol[0]) % 2 === 0) return "even"
-   else return "odd"
-}
+// function characterParity(symbol: string): string {
+//    if(isNaN(parseInt(symbol[0]))) return "not a digit"
+//    else if (parseInt(symbol[0]) % 2 === 0) return "even"
+//    else return "odd"
+// }
 
 console.log(characterParity('5'))
 console.log(characterParity('8'))
 console.log(characterParity('q'))
+
+
+// Second solution which is more concise
+function characterParity(symbol: string): string {
+  const num: any = parseInt(symbol);
+  
+  return isNaN(num) ? 'not a digit' : num % 2 === 0 ? 'even' : 'odd';
+}
