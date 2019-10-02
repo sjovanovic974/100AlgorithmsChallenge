51cd2a13f58789b7fab3b1f426d4acbf26b98d9e
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-02 20:44:36 +0200

    isCaseInsensitivePalindrome Exercise Added

---
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -0,0 +1,10 @@
+// First solution
+function isCaseInsensitivePalindrome(inputString: string): boolean {
+  inputString = inputString.toLowerCase();
+  const reversedString = inputString.split('').reverse().join('');
+
+  return inputString === reversedString;
+}
+
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
