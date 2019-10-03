de4fc51c8fb762fef3a654ccbca401e0212b1d5a
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-03 23:05:38 +0200

    Avoid Obstacles Exercise added (optimized)

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,21 +1,19 @@
-// First solution
+// Optimized version of the first solution
 function avoidObstacles(inputArray: number[]): number {
   const arr: number[] = inputArray.sort((a, b) => a - b);
   
   let leap: number = 0;
-  let i: number = 2; // minimum sequence value to be able to jump over an element
+  let i: number = 2; // minimum value to be able to jump over an element
  
     while(true) {
-      if(arr.every(num => !(num % i === 0))) {
-        leap = i;
-        break;
+      if(arr.every(num => num % i !== 0)) {
+        return leap = i;
       }
       i++;
     }
-
-  return leap;
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
 console.log(avoidObstacles([10, 2, 8, 4, 6]));
-console.log(avoidObstacles([12, 4, 8, 9, 3, 1, 13]));
+console.log(avoidObstacles([12]));
+console.log(avoidObstacles([1, 2, 3, 4, 5, 6, 7]));
