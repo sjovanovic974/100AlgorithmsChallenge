99ba6d254eb41e281851ce5c88c735612f5f1bbd
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-03 23:25:36 +0200

    celsiusToFahrenheit Exercise added

---
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -0,0 +1,8 @@
+// Funny simple one
+function celsiusToFahrenheit(celsius: number): number {
+  return celsius * 9 / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(-30));
+console.log(celsiusToFahrenheit(-10));
+console.log(celsiusToFahrenheit(0));
