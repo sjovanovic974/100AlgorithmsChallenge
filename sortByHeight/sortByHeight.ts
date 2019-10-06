464654148ede232311db5aa0762c7dd5ff25e859
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-06 14:17:33 +0200

    sortByHeight Exercise added (optimized)

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,8 +1,6 @@
-// First solution
+// Second solution, optimized code
 function sortByHeight(a: number[]): number[] {
-  let b: number[] = [...a];
-  b.sort((x, y) => x - y);
-  b = b.filter(num => num !== -1);
+  const b = a.filter(num => num !== -1).sort((x, y) => x - y);
   let i = 0;
 
   return a.map(num => {
@@ -11,3 +9,17 @@ function sortByHeight(a: number[]): number[] {
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+
+// // First solution
+// function sortByHeight(a: number[]): number[] {
+//   let b: number[] = [...a];
+//   b.sort((x, y) => x - y);
+//   b = b.filter(num => num !== -1);
+//   let i = 0;
+
+//   return a.map(num => {
+//     return num === -1 ? num : b[i++];
+//   });
+// }
+
+// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
