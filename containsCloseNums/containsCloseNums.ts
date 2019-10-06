c363cb76e484c8a1348633c37e614f69b62ce889
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-06 21:26:25 +0200

    containsCloseNums Exercise added

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,18 +1,19 @@
-// First solution
+// First proper solution
 function containsCloseNums(nums: number[], k: number): boolean {
   for (let i = 0; i < nums.length; i++) {
-    let idx = i + k;
-
-    if (idx >= nums.length) {
-      return false;
-    }
-
-    if (nums[i] === nums[idx]) {
-      return true;
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[i] === nums[j]) {
+          if (Math.abs(i - j) <= k) {
+            return true;
+          }
+        }
+      }
     }
   }
+  return false;
 }
 
-console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 1], 3));
 console.log(containsCloseNums([0, 1, 2, 1, 3, 5, 3, 5], 2));
-console.log(containsCloseNums([0, 1, 2, 3, 5, 1], 4));
+console.log(containsCloseNums([0, 1, 2, 3, 1, 5], 2));
