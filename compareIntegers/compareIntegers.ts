12daf2b9d7e5c5d913cbd6c912ac4ba94d851ad4
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-06 13:41:02 +0200

    compareIntegers Exercise added

---
+++ compareIntegers/compareIntegers.ts
@@ -0,0 +1,9 @@
+function compareIntegers(a: string, b: string): string {
+  const x = parseInt(a);
+  const y = parseInt(b);
+  return x > y ? 'greater' : x < y ? 'less' : 'equal';
+}
+
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
