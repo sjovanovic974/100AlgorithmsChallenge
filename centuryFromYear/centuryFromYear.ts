38fae3b81fd5aadcb56b694e7b46355c3b902efe
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-07 22:18:25 +0200

    centuryFromYear Exercise added

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,7 +1,7 @@
 // First solution
 function centuryFromYear(year: number): number {
   if (year % 100 === 0) {
-    return Math.floor(year / 100);
+    return year / 100;
   } else {
     return Math.floor(year / 100) + 1;
   }
