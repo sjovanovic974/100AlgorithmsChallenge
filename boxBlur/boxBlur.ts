c264ff18ea022448474b28567c76f9ed91fdc0a5
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-17 19:30:52 +0200

    boxBlur Exercise added

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,4 +1,5 @@
-function boxBlur(image: number[][]): number[][] {
+// I am not sure if I have understood what was task here...
+function boxBlur(image: number[][]): number {
   const arr = image.reduce((acc, el) => acc.concat(el));
   const sum = arr.reduce((acc, num) => (acc += num));
   const length = arr.length;
