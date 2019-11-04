22a7dfcc3fc5a711056b91912d3e5f1248cd74d5
sjovanovic974 <sjovanovic974@gmail.com>
2019-11-04 20:41:25 +0100

    alternatingSums Exercise added

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,3 +1,22 @@
+// Solution using forEach instead of regular for loop
+function alternatingSums(a: number[]): number[] {
+  let teamASum: number = 0;
+  let teamBSum: number = 0;
+
+  a.forEach((num, index) => {
+    if (index % 2 === 0) {
+      teamASum += num;
+    } else {
+      teamBSum += num;
+    }
+  });
+
+  return [teamASum, teamBSum];
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+/*
 // First solution
 function alternatingSums(a: number[]): number[] {
   let teamASum: number = 0;
@@ -15,3 +34,5 @@ function alternatingSums(a: number[]): number[] {
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+*/
