edb921e3932ee4842e6933336e23e1b10973aec6
sjovanovic974 <sjovanovic974@gmail.com>
2019-11-25 17:34:46 +0100

    sumOfTwo Exercise added

---
+++ sumOfTwo/sumOfTwo.js
@@ -0,0 +1,30 @@
+//Second solution
+function sumOfTwo(a, b, v) {
+  const aSet = new Set(a);
+  for (let i = 0; i < b.length; i++) {
+    let match = v - b[i];
+    if (aSet.has(match)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+//O(n)
+
+/**********************************************************
+First solution
+function sumOfTwo(a, b, v) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] + b[j] === v) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+O(n^2) in fact O(a * b)
+************************************************************/
