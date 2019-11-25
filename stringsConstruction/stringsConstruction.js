c2a0d600b62c5fb9dd11ee67c6dea2d26a0f8a76
sjovanovic974 <sjovanovic974@gmail.com>
2019-11-25 12:48:22 +0100

    stringsConstruction Exercise added

---
+++ stringsConstruction/stringsConstruction.js
@@ -0,0 +1,17 @@
+// First solution
+function stringsConstruction(a, b) {
+  const obj = {};
+  for (var i = 0; i < a.length; i++) {
+    obj[a[i]] = 0;
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    if (obj[b[i]] !== undefined) {
+      obj[b[i]]++;
+    }
+  }
+
+  const values = Object.values(obj);
+  return Math.min(...values);
+}
+console.log(stringsConstruction('abc', 'abccba'));
