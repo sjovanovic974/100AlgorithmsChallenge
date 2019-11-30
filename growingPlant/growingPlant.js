9a375930373e65aa454e4560663ff69dac06f622
sjovanovic974 <sjovanovic974@gmail.com>
2019-11-30 23:55:22 +0100

    growingPlant Exercise added

--- growingPlant/growingPlant.js
+++ growingPlant/growingPlant.js
@@ -3,13 +3,12 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
   let day = 0;
   let currentHeight = 0;
   while (currentHeight < desiredHeight) {
-    day++;
     currentHeight += upSpeed;
+    day++;
     if (currentHeight >= desiredHeight) {
       return day;
     }
     currentHeight -= downSpeed;
   }
-  return day;
 }
 console.log(growingPlant(100, 10, 910));
