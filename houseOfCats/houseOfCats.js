7a20a04f4930a24b273bd5a70678bccbc530f0ec
sjovanovic974 <sjovanovic974@gmail.com>
2019-12-31 15:25:23 +0100

    houseOfCats Second solution added

--- houseOfCats/houseOfCats.js
+++ houseOfCats/houseOfCats.js
@@ -1,23 +1,16 @@
-// First solution
+// Second solution
 function houseOfCats(legs) {
   const result = [];
-  const catLegs = 4;
-  const humanlegs = 2;
 
-  if (legs % catLegs === humanlegs) {
+  if (legs % 4 === 2) {
     result.push(1);
+    legs -= 2;
   }
 
-  const range = Math.floor(legs / catLegs);
-
-  for (let i = 1; i <= range; i++) {
-    if (result.length === 1) {
-      result.push(result[0] + humanlegs);
-    } else if (result.length === 0) {
-      result.push(humanlegs);
-    } else {
-      result.push(result[result.length - 1] + humanlegs);
-    }
+  while (legs > 0) {
+    const previousNumber = result[result.length - 1] || 0;
+    result.push(previousNumber + 2);
+    legs -= 4;
   }
   return result;
 }
@@ -25,3 +18,28 @@ function houseOfCats(legs) {
 console.log(houseOfCats(6));
 console.log(houseOfCats(2));
 console.log(houseOfCats(18));
+console.log(houseOfCats(8));
+
+// First solution
+// function houseOfCats(legs) {
+//   const result = [];
+//   const catLegs = 4;
+//   const humanlegs = 2;
+
+//   if (legs % catLegs === humanlegs) {
+//     result.push(1);
+//   }
+
+//   const range = Math.floor(legs / catLegs);
+
+//   for (let i = 1; i <= range; i++) {
+//     if (result.length === 1) {
+//       result.push(result[0] + humanlegs);
+//     } else if (result.length === 0) {
+//       result.push(humanlegs);
+//     } else {
+//       result.push(result[result.length - 1] + humanlegs);
+//     }
+//   }
+//   return result;
+// }
