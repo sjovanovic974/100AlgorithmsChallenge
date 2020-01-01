5b7abd53e5b8796559ba6c6f2d14951a305165b8
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-01 21:43:33 +0100

    extractEachKth Exercise added

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,6 +1,6 @@
 // First solution
 function extractEachKth(inputArray: number[], k: number): number[] {
-  return inputArray.filter(num => num % 3 !== 0);
+  return inputArray.filter((num, idx) => (idx + 1) % 3 !== 0);
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
