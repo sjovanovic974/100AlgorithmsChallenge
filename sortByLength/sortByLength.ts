79aeac7edef99bc3d0bc4adebda06a2532b2146f
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-01 20:55:39 +0100

    sortByLength Exercise added

---
+++ sortByLength/sortByLength.ts
@@ -0,0 +1,6 @@
+// First solution
+function sortByLength(inputArray: string[]): string[] {
+  return inputArray.sort((a, b) => a.length - b.length);
+}
+
+console.log(sortByLength(['abc', '', 'aaa', 'a', 'zz']));
