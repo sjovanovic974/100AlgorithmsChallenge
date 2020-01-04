213e79d20c676182f8c0fe5e26fe2cb77b2fa63a
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-04 16:09:26 +0100

    pagesNumberingWithInk second solution added

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -3,20 +3,34 @@ function pagesNumberingWithInk(
   current: number,
   numberOfDigits: number
 ): number {
-  let currentLength: number = current.toString().length;
-  let nextNumber: number = current + 1;
-  const pages: number[] = [];
-
-  while (currentLength <= numberOfDigits) {
-    pages.push(current);
-    numberOfDigits -= currentLength;
-    current = nextNumber;
-    currentLength = current.toString().length;
-    nextNumber = current + 1;
+  let result = 0;
+  while (current.toString().length <= numberOfDigits) {
+    numberOfDigits -= current.toString().length;
+    result = current;
+    current++;
   }
-  return pages.pop();
+  return result;
 }
 
 console.log(pagesNumberingWithInk(21, 5));
 console.log(pagesNumberingWithInk(8, 4));
 console.log(pagesNumberingWithInk(1, 5));
+
+// First solution
+// function pagesNumberingWithInk(
+//   current: number,
+//   numberOfDigits: number
+// ): number {
+//   let currentLength: number = current.toString().length;
+//   let nextNumber: number = current + 1;
+//   const pages: number[] = [];
+
+//   while (currentLength <= numberOfDigits) {
+//     pages.push(current);
+//     numberOfDigits -= currentLength;
+//     current = nextNumber;
+//     currentLength = current.toString().length;
+//     nextNumber = current + 1;
+//   }
+//   return pages.pop();
+// }
