deb44c95f031c97b47d29cb4ae97a1a7dcd69eab
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-06 23:01:36 +0100

    shapeArea Second solution added

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,20 +1,33 @@
+// Second solution
 function shapeArea(n: number): number {
-  if (n < 1) return 0;
-
-  let result: number = 0;
-  let start: number = 0;
-  let follow: number = 4;
+  let total = 1;
 
   for (let i = 1; i < n; i++) {
-    result += start + follow;
-    const temp = start;
-    start = follow;
-    follow = temp + start;
+    total += i * 4;
   }
 
-  return result + 1;
+  return total;
 }
+
 console.log(shapeArea(1));
 console.log(shapeArea(2));
 console.log(shapeArea(3));
 console.log(shapeArea(4));
+
+// First solution
+// function shapeArea(n: number): number {
+//   if (n < 1) return 0;
+
+//   let result: number = 0;
+//   let start: number = 0;
+//   let follow: number = 4;
+
+//   for (let i = 1; i < n; i++) {
+//     result += start + follow;
+//     const temp = start;
+//     start = follow;
+//     follow = temp + start;
+//   }
+
+//   return result + 1;
+// }
