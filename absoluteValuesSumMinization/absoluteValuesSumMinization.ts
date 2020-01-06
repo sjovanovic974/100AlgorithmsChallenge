7220321fd9e81100c3d8882b688114d96798c0f6
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-07 00:06:48 +0100

    absoluteValuesSumMinization Exercise added

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,22 @@
+// First solution
 function absoluteValuesSumMinimization(a: number[]): number {
+  let min = a.reduce((sum, num) => sum + num);
+  let index = -1;
 
+  for (let i = 0; i < a.length; i++) {
+    let result = 0;
+    for (let x = 0; x < a.length; x++) {
+      result += Math.abs(a[i] - a[x]);
+    }
+    if (result < min) {
+      min = result;
+      index = a[i];
+    }
+  }
+  return index;
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
