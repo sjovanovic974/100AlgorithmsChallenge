cd9467ce835c65a8327f53dce6f24b3951d96f64
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-06 22:08:05 +0100

    shapeArea Exercise added

---
+++ shapeArea/shapeArea.ts
@@ -0,0 +1,20 @@
+function shapeArea(n: number): number {
+  if (n < 1) return 0;
+
+  let result: number = 0;
+  let start: number = 0;
+  let follow: number = 4;
+
+  for (let i = 1; i < n; i++) {
+    result += start + follow;
+    const temp = start;
+    start = follow;
+    follow = temp + start;
+  }
+
+  return result + 1;
+}
+console.log(shapeArea(1));
+console.log(shapeArea(2));
+console.log(shapeArea(3));
+console.log(shapeArea(4));
