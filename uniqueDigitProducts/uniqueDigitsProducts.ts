647e70925b383451afdc5107fee5767ddedcf7c4
sjovanovic974 <sjovanovic974@gmail.com>
2020-01-07 13:33:10 +0100

    uniqueDigitProducts Exercise added

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,21 +1,45 @@
-// First solution
+// Second solution
 function uniqueDigitProducts(a: number[]): number {
   const products: number[] = [];
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] < 10) {
-      products.push(a[i]);
-    } else {
-      const stringNumber: String = a[i].toString();
-      let product: number = 1;
-      for (let x = 0; x < stringNumber.length; x++) {
-        product *= parseInt(stringNumber[x]);
-      }
+  for (let num of a) {
+    const product: number = digitsProduct(num);
+    if (!products.includes(product) {
       products.push(product);
     }
   }
 
-  return new Set(products).size;
+  return products.length;
+}
+
+function digitsProduct(num: number): number {
+  const strNum = num.toString();
+  let product: number = 1;
+
+  for (let i = 0; i < strNum.length; i++) {
+    product *= parseInt(strNum[i]);
+  }
+  return product;
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+
+// First solution
+// function uniqueDigitProducts(a: number[]): number {
+//   const products: number[] = [];
+
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] < 10) {
+//       products.push(a[i]);
+//     } else {
+//       const stringNumber: String = a[i].toString();
+//       let product: number = 1;
+//       for (let x = 0; x < stringNumber.length; x++) {
+//         product *= parseInt(stringNumber[x]);
+//       }
+//       products.push(product);
+//     }
+//   }
+
+//   return new Set(products).size;
+// }
